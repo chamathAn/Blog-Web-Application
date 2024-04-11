bd5536bfe9f1eb38a126dee4c2657635b5e7c9cd
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2024-04-11 14:12:23 +0530

    ..

--- api/index.js
+++ api/index.js
@@ -1,48 +1,74 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose'); // Corrected import
-const User = require('./model/User');
-const bcrypt = require('bcrypt');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "434324324fdsfdsfsdf"
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+));
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://chamathanjula2:tMxY4nOqDr3ZIApi@blog-site.kqillr9.mongodb.net/?retryWrites=true&w=majority&appName=blog-site')
-    .then(() => {
-        console.log('MongoDB connected');
-    })
-    .catch((error) => {
-        console.error('MongoDB connection error:', error);
-    });
+mongoose
+  .connect(
+    "mongodb+srv://chamathanjula2:tMxY4nOqDr3ZIApi@blog-site.kqillr9.mongodb.net/?retryWrites=true&w=majority&appName=blog-site"
+  )
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
-app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const userDoc = await User.create({
-            username,
-            password: bcrypt.hashSync(password, 12)
-        });
-        res.send(userDoc);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, 12),
+    });
+    res.send(userDoc);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Registration failed");
+  }
 });
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const userDoc = await User.findOne({
-            username,
-        });
-
-        
-        res.send(userDoc);
-    } catch (error) {
-        res.status(400).send(error);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.findOne({ username });
+    if (!userDoc) {
+      return res.status(400).send("User not found");
     }
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (!passOk) {
+      return res.status(401).send("Invalid password");
+    }
+    jwt.sign(
+      { username, id: userDoc._id },
+      JWT_SECRET,
+      {},
+      (err, token) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Internal server error");
+        }
+        res.cookie("auth-token", token).json("Login successful");
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Login failed");
+  }
 });
 
 app.listen(4000, () => {
-    console.log('app listening on port 4000!');
+  console.log("app listening on port 4000!");
 });
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,29 +1,56 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
-export default function Login(){
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useStatetate('');
-    function login(){
-        fetch('http://localhost:4000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        })
-    }
-    return(
-      
-           <form className="login" onSubmit={login}>
-            <h1>Login</h1>
-            <input type="text" placeholder="username" value={username} onChange={e=> setUsername(e.target.value)} />
-            <input type="password" placeholder="password "value={password} onChange={e=> setPassword(e.target.value)} />
-            <button>Login</button>
-           </form>
-       
-    )
+  function login(e) {
+    e.preventDefault();
+    axios
+      .post(
+        "http://localhost:4000/login",
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setRedirect(true);
+        } else {
+          alert("Wrong credentials");
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  if (redirect) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <form className="login" onSubmit={login}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
